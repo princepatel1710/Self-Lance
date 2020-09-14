c171e697842039c64ad1fb0015261fab4fd001f9
Deeksha <deeksha6624@gmail.com>
2020-09-13 21:16:32 -0400

    first

---
+++ app/src/main/java/com/example/selflance/HireAFreelancer.java
@@ -0,0 +1,14 @@
+package com.example.selflance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HireAFreelancer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hire_a_freelancer);
+    }
+}
