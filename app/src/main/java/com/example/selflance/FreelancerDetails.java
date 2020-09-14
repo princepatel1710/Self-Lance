d0967bc369aad8c4fea6f6dcf0bfc3cabbd9d485
Deeksha <deeksha6624@gmail.com>
2020-09-14 17:29:32 -0400

    first

--- app/src/main/java/com/example/selflance/FreelancerDetails.java
+++ app/src/main/java/com/example/selflance/FreelancerDetails.java
@@ -3,14 +3,12 @@ package com.example.selflance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class FreelancerDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_freelancer_details);
     }
-
 }
