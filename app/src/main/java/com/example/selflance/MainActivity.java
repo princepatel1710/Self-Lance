e89268b15db2618135be00c2465fff5a0201d25e
Deeksha <deeksha6624@gmail.com>
2020-09-21 19:33:07 -0400

    first

--- app/src/main/java/com/example/selflance/MainActivity.java
+++ app/src/main/java/com/example/selflance/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.selflance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.lookingforjob);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LookingForJob.class));
+            }
+        });
     }
+
 
 }
