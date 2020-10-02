2a6a953a151bd2c1c9a624f0aeb1a12b1b74fb9a
Deeksha <deeksha6624@gmail.com>
2020-10-02 18:50:34 -0400

    first Commit

--- app/src/main/java/com/example/selflance/LookingForJob.java
+++ app/src/main/java/com/example/selflance/LookingForJob.java
@@ -2,13 +2,24 @@ package com.example.selflance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LookingForJob extends AppCompatActivity {
-
+TextView job1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looking_for_job);
+
+        job1 = findViewById(R.id.job1);
+        job1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LookingForJobDescription.class));
+            }
+        });
     }
 }
